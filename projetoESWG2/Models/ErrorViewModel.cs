ea17c8864bf2000d31293746523db31b20c9ede3
Miguel Gomes <gomesmike24@hotmail.com>
2018-11-12 19:35:37 +0000

    Add project files.

---
+++ projetoESWG2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projetoESWG2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
