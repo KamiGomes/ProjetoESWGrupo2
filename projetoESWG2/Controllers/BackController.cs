187ddd42ca2730b41ad60e74a8b821c1a53c38a3
Miguel Gomes <gomesmike24@hotmail.com>
2018-11-12 21:21:20 +0000

    Criação do Projeto Base - Adição de Pastas para Backend

---
+++ projetoESWG2/Controllers/BackController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projetoESWG2.Controllers
+{
+    public class BackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
