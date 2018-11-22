aa4d625c6e48ce6d6d65df2d71de6f517fee510b
Unknown <rafael.botas@libretrend.com>
2018-11-22 00:47:25 +0000

    Migration to seed Roles table
    JUST DO A update-database

---
+++ ESW_Shelter/Data/Migrations/20181122004412_seed_roles_table.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ESW_Shelter.Data.Migrations
+{
+    public partial class seed_roles_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleName" },
+                values: new[] { "Client" }
+                );
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleName" },
+                values: new[] { "Volunteer" }
+                );
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleName" },
+                values: new[] { "Employee" }
+                );
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleName" },
+                values: new[] { "Administrator" }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumns: new[] { "RoleName" },
+                keyValues: new[] { "Client" }
+                );
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumns: new[] { "RoleName" },
+                keyValues: new[] { "Volunteer" }
+                );
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumns: new[] { "RoleName" },
+                keyValues: new[] { "Employee" }
+                );
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumns: new[] { "RoleName" },
+                keyValues: new[] { "Administrator" }
+                );
+        }
+    }
+}
