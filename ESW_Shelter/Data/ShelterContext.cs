bdf659ba7199875ac9ab4f077c629f10ff4fe5c4
Miguel Gomes <gomesmike24@hotmail.com>
2018-12-13 04:53:21 +0000

    Revert "Upgraded 2.2Core (All Updates Done)"
    This reverts commit a8d74d27a7a215150c1b6bcd6c918547b22c8c2a.

--- ESW_Shelter/Data/ShelterContext.cs
+++ ESW_Shelter/Data/ShelterContext.cs
@@ -14,6 +14,7 @@ namespace ESW_Shelter.Models
         }
 
         public DbSet<ESW_Shelter.Models.Users> Users { get; set; }
+        public DbSet<ESW_Shelter.Models.UsersInfo> UsersInfo { get; set; }
         public DbSet<ESW_Shelter.Models.Roles> Roles { get; set; }
 
     }
