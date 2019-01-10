be8d9ac10cacbdb109f277e129975eeeb2610fe8
Unknown <rafaelalexandre.botasrosado@gmail.com>
2019-01-10 00:43:08 +0000

    Stripe Customer registering implementation

---
+++ ESW_Shelter/Libs/Stripe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stripe;
+using ESW_Shelter.Models;
+
+namespace ESW_Shelter.Libs
+{
+    public class StripeLib
+    {
+        async public void CreateCustomer(Models.Users user)
+        {
+            Dictionary<string, string> metadata = new Dictionary<string, string>() {
+                { "userId", user.UserID.ToString()}
+            };
+
+            CustomerCreateOptions customerOpts = new CustomerCreateOptions
+            {
+                Email = user.Email,
+                Metadata = metadata
+            };
+
+            Stripe.CustomerService customerService = new Stripe.CustomerService();
+            Stripe.Customer newCustomer = await customerService.CreateAsync(customerOpts);
+        }
+
+    }
+}
