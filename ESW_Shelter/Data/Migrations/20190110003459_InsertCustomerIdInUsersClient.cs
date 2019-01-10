5b53935177d69164187ae78832f66ae200ccf96d
Unknown <rafaelalexandre.botasrosado@gmail.com>
2019-01-10 00:41:12 +0000

    Migration to add Stripe's Customer Id column into Users

---
+++ ESW_Shelter/Data/Migrations/20190110003459_InsertCustomerIdInUsersClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ESW_Shelter.Data.Migrations
+{
+    public partial class InsertCustomerIdInUsersClient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>("customerId","Users", nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("customerId", "Users");
+        }
+    }
+}
