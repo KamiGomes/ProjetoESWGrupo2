3455ebf8982af09c2f73ffb2b5c798dbbfc8485f
Unknown <rafaelalexandre.botasrosado@gmail.com>
2019-01-10 00:42:26 +0000

    Created DonationsController

---
+++ ESW_Shelter/Controllers/DonationsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ESW_Shelter.Models;
+
+namespace ESW_Shelter.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private readonly ShelterContext _context;
+
+        public DonationsController(ShelterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Donations
+        public async Task<IActionResult> Index()
+        {
+            var service = new Stripe.PlanService();
+            var options = new Stripe.PlanListOptions { };
+
+            Stripe.StripeList<Stripe.Plan> plans = service.List(options);
+
+            foreach (Stripe.Plan plan in plans)
+            {
+                
+            }
+            
+            return View();
+        }
+
+        // GET: Donations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donation = await _context.Donation
+                .FirstOrDefaultAsync(m => m.DonationID == id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+
+            return View(donation);
+        }
+
+        // GET: Donations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DonationID,Name,Quantity,AnimalTypeFK,ProductTypeFK,UserIDFK")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(donation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donation = await _context.Donation.FindAsync(id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+            return View(donation);
+        }
+
+        // POST: Donations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DonationID,Name,Quantity,AnimalTypeFK,ProductTypeFK,UserIDFK")] Donation donation)
+        {
+            if (id != donation.DonationID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(donation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DonationExists(donation.DonationID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donation = await _context.Donation
+                .FirstOrDefaultAsync(m => m.DonationID == id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+
+            return View(donation);
+        }
+
+        // POST: Donations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var donation = await _context.Donation.FindAsync(id);
+            _context.Donation.Remove(donation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DonationExists(int id)
+        {
+            return _context.Donation.Any(e => e.DonationID == id);
+        }
+    }
+}
