dc7c8e3e0723279151511b84050227541f5af375
David Afonso <davidmfa@hotmail.com>
2019-01-30 12:07:01 +0000

    update tester

---
+++ AnimalRaceControllerTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AnimalRaceControllerTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
